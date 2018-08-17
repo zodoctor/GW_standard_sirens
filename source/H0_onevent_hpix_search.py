4a65b4e2434f9e4aad1787186ebf72f783ed9350
apalmese <antonella.palmese@gmail.com>
2018-08-16 19:52:47 -0500

    H0_onevent_hpix_search.py made clearer

--- source/H0_onevent_hpix_search.py
+++ source/H0_onevent_hpix_search.py
@@ -6,34 +6,54 @@ import numpy as np
 from scipy.constants import c #m/s
 from scipy.stats import norm
 import posterior_utils as pos
+import argparse
+import os
 
-def lnlike(H0, z, pb_gal, distmu, diststd, distnorm, pixarea):
-	distgal = (c/1000.)*z/H0
-	like_gals = pb_gal * distnorm * norm(distmu, diststd).pdf(distgal)*z**2  #*distgal**2  #Maybe this is not clever as it takes a log of exp...
-	normalization = H0**3
-	return np.log(np.sum(like_gals)/normalization)
 
-def lnprior(H0):
-	if 10. < H0 < 220. :
-		return 0.0
-	return -np.inf
+parser = argparse.ArgumentParser(description='Compute H0 posterior given a GW event and a galaxy catalog given in fits files for each healpy pixel')
 
-def lnprob(H0, z, pb_gal, distmu, diststd, distnorm, pixarea):
-	lp = lnprior(H0)
-	if not np.isfinite(lp):
-		return -np.inf
-	return lp + lnlike(H0, z, pb_gal, distmu, diststd, distnorm, pixarea)
+parser.add_argument('--incat', default='DES_GW170814_galaxies/',
+                    help='Input galaxy catalog directory path')
+parser.add_argument('--skymap', default='skymap.fits',
+                    help='Input skymap')
+parser.add_argument('--zmin', default=0.07,
+                    help='Minimum redshift to be considered in the galaxy catalog. Default is for GW170814.')
+parser.add_argument('--zmax', default=0.14,
+                    help='Maximum redshift to be considered in the galaxy catalog. Default is for GW170814.')
+parser.add_argument('--Hmin', default=10.,
+                    help='Minimum H0 for a flat prior.')
+parser.add_argument('--Hmax', default=220.,
+                    help='Maximum H0 for a flat prior.')
 
- 
-pb_frac = 0.9
-NSIDE=1024
-H0bins = 100 
-INPUT_DIR = "../catalogs/DES_GW170814_galaxies/" #/data/des60.b/data/palmese/GW_reject_cat/DES_GW170814
+
+args = parser.parse_args()
+
+incat = args.incat
+skymap = args.skymap
+z_min = args.zmin
+z_max = args.zmax
+H0_min = args.Hmin
+H0_max = args.Hmax
+
+pb_frac = 0.9 #Fraction of the skymap prbability to consider, decrease for speed
+NSIDE = 1024     #skymap nside, corresponding also to the nside of the hpix column in the galaxy catalog
+H0bins = 100
+out_plot = 'H0_posterior_GW170814.png'
+z_column_name = 'DNF_ZMEAN_MOF'
+zerr_column_name = 'DNF_ZSIGMA_MOF'
+
+
+DIR_SOURCE = os.getcwd()
+DIR_MAIN = DIR_SOURCE.rsplit('/', 1)[0]
+DIR_CATALOG = incat #DIR_MAIN+'/catalogs/'+incat
+DIR_SKYMAP = DIR_MAIN+'/skymaps/'
+DIR_PLOTS = DIR_MAIN+'/plots/'
+
 
 #For now I am degrading the map to match nside frm the DES catalog
 
 print "Reading skymap..."
-map = fits.open('../skymaps/skymap.fits')[1].data
+map = fits.open(DIR_SKYMAP+skymap)[1].data
 pb = map['PROB']
 distmu = map['DISTMU']
 distsigma = map['DISTSIGMA']
@@ -61,19 +81,19 @@ z_gal , zerr_gal, pb_gal, distmu_gal, distsigma_gal, distnorm_gal = [], [], [], 
 
 for thisField in idx_sort_cut_nside32:
 	if thisField < 10000:
-		filename  = INPUT_DIR + "GW_cat_hpx_0" + str(thisField) + ".fits"
+		filename  = DIR_CATALOG + "GW_cat_hpx_0" + str(thisField) + ".fits"
 	else:
-		filename  = INPUT_DIR + "GW_cat_hpx_" + str(thisField) + ".fits"
+		filename  = DIR_CATALOG + "GW_cat_hpx_" + str(thisField) + ".fits"
 	#print filename+'\n'
 	h = fits.open(filename)[1].data
-	mask_good = ((h['DNF_ZMEAN_MOF']>0.04) & (h['DNF_ZMEAN_MOF']<0.17))
+	mask_good = ((h[z_column_name]>z_min) & (h[z_column_name]<z_max))
 	ra_gal=h['RA_1'][mask_good]
 	dec_gal=h['DEC_1'][mask_good]
 	phi_gal = ra_gal*pi/180.
 	theta_gal = (90.-dec_gal)*pi/180.
 	pix_gal = hp.pixelfunc.ang2pix(NSIDE, theta_gal, phi_gal,nest=True) #Check if it should have been nest=True
-	z_gal.append(h['DNF_ZMEAN_MOF'][mask_good])
-	zerr_gal.append(h['DNF_ZSIGMA_MOF'][mask_good])
+	z_gal.append(h[z_column_name][mask_good])
+	zerr_gal.append(h[zerr_column_name][mask_good])
 	pb_gal.append(pb[pix_gal])
 	distmu_gal.append(distmu[pix_gal])
 	distsigma_gal.append(distsigma[pix_gal])
@@ -88,7 +108,6 @@ distnorm_gal = np.concatenate(distnorm_gal)
 
 #Posterior without normalization at the moment, and with a delta function for z
 
-
 H0_array = np.linspace(10.,220.,num=H0bins)
 lnposterior=[]
 pixarea = hp.nside2pixarea(NSIDE )
@@ -96,8 +115,7 @@ pixarea = hp.nside2pixarea(NSIDE )
 
 print "Estimating Posterior for H0 values:"
 for i in range(H0bins):
-	#print H0_array[i]
-	lnposterior.append(lnprob(H0_array[i], z_gal, pb_gal, distmu_gal, distsigma_gal, distnorm_gal, pixarea))
+	lnposterior.append(pos.lnprob(H0_array[i], z_gal, zerr_gal, pb_gal, distmu_gal, distsigma_gal, distnorm_gal, pixarea, H0_min, H0_max))
 
 posterior = np.exp(lnposterior)
 plt.ion()
@@ -107,17 +125,16 @@ plt.plot(H0_array, np.exp(lnposterior))
 plt.xlabel('$H_0$ [km/s/Mpc]',fontsize=20)
 plt.ylabel('$p$',fontsize=20)
 plt.tight_layout()
-#plt.show()
-plt.savefig('../plots/H0_posterior_GW170814.png')
+plt.savefig(DIR_PLOTS+out_plot)
 
-idx_max = np.argmax(lnposterior)
+idx_max = np.argmax(posterior)
 
 perc_max = posterior[:idx_max].sum()/posterior.sum()
 
-maxposterior = posterior[np.argmax(lnposterior)]
+maxposterior = posterior[idx_max]
 
 print "ML percentile: ", perc_max
-print "H0 ML: ", H0_array[idx_max], "+", pos.percentile(perc_max+0.34, posterior, H0_array)-H0_array[idx_max], "-", H0_array[idx_max] - pos.percentile(perc_max-0.34, posterior, H0_array)
+print "H0 max posterior: ", H0_array[idx_max], "+", pos.percentile(perc_max+0.34, posterior, H0_array)-H0_array[idx_max], "-", H0_array[idx_max] - pos.percentile(perc_max-0.34, posterior, H0_array)
 print "H0 Median: ", pos.percentile(0.50, posterior, H0_array)
 
 
